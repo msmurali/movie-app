c02214fe19448295cfbd5ee5ea6976914ec6dec1
msmurali <muralikrishnan0511@gmail.com>
2021-02-05 11:09:38 +0530

    Updates

---
+++ precache-manifest.46c843844f3beac777ad90a72e66dcf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445e81e7cecf48c787d1db1990a883d1",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "849c7230e23ae50a790e",
+    "url": "/movie-app/static/css/main.bde5afe6.chunk.css"
+  },
+  {
+    "revision": "53dcc3c006582ae85854",
+    "url": "/movie-app/static/js/2.d3900fa6.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/movie-app/static/js/2.d3900fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849c7230e23ae50a790e",
+    "url": "/movie-app/static/js/main.f95c26cd.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
